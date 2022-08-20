c95e2acdc2cfff697b2beeb35844953d317417be
ankur <ankurchurasiya8871@gmail.com>
2022-08-20 15:01:49 +0530

    bv

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+var swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 2,
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+
+
+$(document).ready(function () {
+  $(window).bind('scroll', function () {
+    var navhight = $(window).height() - 0
+    if ($(window).scrollTop() > 70) {
+      $('nav').addClass('fixed-top');
+    }
+    else {
+      $('nav').removeClass('fixed-top');
+    }
+  });
+
+  $(".project").hover3d({
+    selector: ".project__card",
+    shine: true,
+  });
+});
+$(".top").click(function () {
+$("html, body").animate({
+scrollTop: 0
+}, 0)
+})
